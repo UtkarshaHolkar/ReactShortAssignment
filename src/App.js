524785caf05f6bf8d68834e56a599b3889f798d1
UtkashaHolakr <utkarshaaholkar@gmail.com>
2023-12-02 14:11:14 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-
+// src/App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import 'animate.css'; 
+import 'animate.css';
 import './App.css';
+
 const App = () => {
-  const [userData, setUserData] = useState(null);
+  const [userList, setUserList] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://randomuser.me/api/?page=1&results=1&seed=abc');
-        setUserData(response.data.results[0]);
+        const response = await axios.get('https://randomuser.me/api/?page=1&results=6&seed=abc');
+        setUserList(response.data.results);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -20,30 +21,26 @@ const App = () => {
   }, []);
 
   return (
-    
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-blue-500 to-blue-700 p-4">
-    {userData && (
-      <div className="w-full max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeInUp p-8">
-    <div className="container mx-auto p-4">
-      {userData && (
-        <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md animate__animated animate__fadeInUp">
-          <img
-            className="w-full h-64 object-cover animate__animated animate__fadeIn"
-            src={userData.picture.large}
-            alt="User"
-          />
-          <div className="p-6">
-            <h2 className="font-bold text-xl mb-2 animate__animated animate__fadeIn">
-              {userData.name.first} {userData.name.last}
-            </h2>
-            <p className="text-gray-600 animate__animated animate__fadeIn">{userData.email}</p>
-            <p className="text-gray-600 animate__animated animate__fadeIn">{userData.phone}</p>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-500 to-blue-700 p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        {userList.map((user, index) => (
+          <div key={index} className="bg-white rounded-md overflow-hidden shadow-md p-8 animate__animated animate__fadeInUp">
+            <div className="flex">
+              <div className="w-1/3">
+                <img className="w-full h-40 object-cover mb-4" src={user.picture.large} alt={`User ${index + 1}`} />
+              </div>
+              <div className="w-2/3 pl-4">
+                <h2 className="font-bold text-xl mb-2">
+                  {user.name.first} {user.name.last}
+                </h2>
+                <p className="text-gray-600">Gender: {user.gender}</p>
+                <p className="text-gray-600">Email: {user.email}</p>
+                <p className="text-gray-600">Phone: {user.phone}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
-    </div>
-    )}
+        ))}
+      </div>
     </div>
   );
 };
