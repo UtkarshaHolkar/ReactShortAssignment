27f0353a8031dc84db973c3542088ee9f4f11720
UtkashaHolakr <utkarshaaholkar@gmail.com>
2023-12-02 13:28:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'animate.css'; // Import animate.css
+import './App.css'; // You can import Tailwind CSS here if using
 
-function App() {
+const App = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://randomuser.me/api/?page=1&results=1&seed=abc');
+        setUserData(response.data.results[0]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto p-4">
+      {userData && (
+        <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md animate__animated animate__fadeInUp">
+          <img
+            className="w-full h-64 object-cover animate__animated animate__fadeIn"
+            src={userData.picture.large}
+            alt="User"
+          />
+          <div className="p-6">
+            <h2 className="font-bold text-xl mb-2 animate__animated animate__fadeIn">
+              {userData.name.first} {userData.name.last}
+            </h2>
+            <p className="text-gray-600 animate__animated animate__fadeIn">{userData.email}</p>
+            <p className="text-gray-600 animate__animated animate__fadeIn">{userData.phone}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
